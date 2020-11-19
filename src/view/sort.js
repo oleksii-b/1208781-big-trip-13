20353afbfcfc96a4e36658709e906909b97eef59
Тимур <rafegg21@gmail.com>
2020-11-19 22:31:28 +0500

    Отрисованы меню, сортировка, формы создания и редактирования и три точки маршрута в списке

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,5 +1,6 @@
 export const createSortTemplate = () => {
-  return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+  return `<h2 class="visually-hidden">Trip events</h2>
+  <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
   <div class="trip-sort__item  trip-sort__item--day">
     <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
     <label class="trip-sort__btn" for="sort-day">Day</label>
