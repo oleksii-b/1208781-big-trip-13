7bfea61a8dbc14856be1faf8ef7879e977e34843
Рафиков Тимур Ильдарович <trafikov@rtrn.ru>
2020-11-25 14:40:56 +0500

    Скрытие контейнера при остуствии offers

--- src/view/edit-point.js
+++ src/view/edit-point.js
@@ -106,15 +106,8 @@ export const createEditPointTemplate = (event) => {
       </button>
     </header>
     <section class="event__details">
-      <section class="event__section  event__section--offers">
-        <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
-        <div class="event__available-offers">
-        ${offerTemplate}
-        </div>
-      </section>
-
-      <section class="event__section  event__section--destination">
+       ${offerTemplate} 
+    <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
         <p class="event__destination-description">${description}</p>
       </section>
--- src/view/new-point.js
+++ src/view/new-point.js
@@ -7,15 +7,20 @@ const createPhotoTemplate = (photos) => {
 };
 
 export const createOfferTemplate = (offers) => {
-  return offers.map((offer) =>
+  return `<section class="event__section  event__section--offers">
+  <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+  <div class="event__available-offers">
+    ${offers.map((offer) =>
     `<div class="event__offer-selector">
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train" ${getRandomInteger(0, 1) ? `checked` : ``}>
-      <label class="event__offer-label" for="event-offer-train-1">
-      <span class="event__offer-title">${offer.name}</span>
-      &plus;&euro;&nbsp;
-      <span class="event__offer-price">${offer.price}</span>
-      </label>
-    </div>`).join(``);
+        <input class="event__offer-checkbox  visually-hidden" id="event-offer-train-1" type="checkbox" name="event-offer-train" ${getRandomInteger(0, 1) ? `checked` : ``}>
+        <label class="event__offer-label" for="event-offer-train-1">
+        <span class="event__offer-title">${offer.name}</span>
+        &plus;&euro;&nbsp;
+        <span class="event__offer-price">${offer.price}</span>
+        </label>
+      </div>`).join(``)}
+  </div>
+</section>`;
 };
 
 export const createCityTemplate = (cities) => {
@@ -123,13 +128,7 @@ export const createNewPointTemplate = (event) => {
       <button class="event__reset-btn" type="reset">Cancel</button>
     </header>
     <section class="event__details">
-      <section class="event__section  event__section--offers">
-        <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
-        <div class="event__available-offers">
-          ${offerTemplate}
-        </div>
-      </section>
+        ${offerTemplate}
 
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
