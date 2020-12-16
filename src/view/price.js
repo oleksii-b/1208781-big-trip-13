f52230fb0d5bf5cfda64c912d41f0a0004994d90
Рафиков Тимур Ильдарович <trafikov@rtrn.ru>
2020-12-16 16:10:38 +0500

    Исправляет отображение цены

--- src/view/price.js
+++ src/view/price.js
@@ -2,7 +2,10 @@ import AbstractView from './abstract';
 
 const createPriceTemplate = (events) => {
   const totalPrice = events.reduce((acc, event) => acc + event.price, 0);
-  const totalPriceOffers = events.reduce((acc1, elem) => acc1 + elem.offers.reduce((acc, offer) => acc + +offer.price, 0), 0);
+  const totalPriceOffers = events
+                      .reduce((acc1, event) => acc1 + event.offers
+                      .filter((offer) => offer.checked && event.eventType === offer.id)
+                      .reduce((acc, offer) => acc + +offer.price, 0), 0);
   const totalCoast = totalPrice + totalPriceOffers;
   return (
     `<p class="trip-info__cost">
